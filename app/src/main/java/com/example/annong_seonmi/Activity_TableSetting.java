91e98c7b63f11a1b479d6fc108dfcfa5aa987221
seonmi <rachel0238@gmail.com>
2022-12-27 07:23:36 +0900

    데이터가 필요해

--- app/src/main/java/com/example/annong_seonmi/Activity_TableSetting.java
+++ app/src/main/java/com/example/annong_seonmi/Activity_TableSetting.java
@@ -1,151 +1,159 @@
 package com.example.annong_seonmi;
 
-import android.app.Dialog;
-import android.content.Context;
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
-import android.content.res.AssetManager;
-import android.graphics.Color;
-import android.graphics.Point;
 import android.os.Bundle;
-import android.renderscript.ScriptGroup;
-import android.service.autofill.FieldClassification;
-import android.text.PrecomputedText;
-import android.util.DisplayMetrics;
 import android.util.Log;
-import android.util.TypedValue;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.Spinner;
 import android.widget.TableLayout;
 import android.widget.TableRow;
-import android.widget.Toast;
+import android.widget.TextView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.lang.reflect.Parameter;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Activity_TableSetting extends AppCompatActivity {
 
-    Spinner spinner_crops_name;
+    TextView textView_crop_name;
     TableLayout table_layout;
-    Button btn_add_row;
+    Button btn_add_row, btn_delete_row;
+
+    String crop_name;
+
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_table_setting);
 
-        /* 작물 리스트 불러오기 */
-        ArrayList<String> crops_name_list = new ArrayList<>();
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(getAssets().open("crops_name.txt")));
-            String crops_name = reader.readLine();
-            while(crops_name != null) {
-                crops_name_list.add(crops_name);
-                crops_name = reader.readLine();
-            }
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }        spinner_crops_name = (Spinner) findViewById(R.id.spinner_crops_name);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, androidx.constraintlayout.widget.R.layout.support_simple_spinner_dropdown_item, crops_name_list);
-        spinner_crops_name.setAdapter(arrayAdapter);
+        textView_crop_name = (TextView) findViewById(R.id.textView_crop_name);
 
-        /* 새 작물 추가 */
-        spinner_crops_name.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                if(i == 0) {
-                    
-                }
-            }
+        /* 인텐트 시작 -> 새 인텐트에서 종료 시 작물 이름 반환 -> 작물 이름 확인 */
+        intent = new Intent(Activity_TableSetting.this, activity_select_crop_name.class);
+        intent.putExtra("init", "setting_table");
+//        startActivityForResult(intent, 1);
+        activityResultLauncher.launch(intent);
 
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
 
-            }
-        });
 
-        /* 테이블 추가 */
+        check_table_data();
+
+
         table_layout = (TableLayout) findViewById(R.id.table_layout);
+
+        /* 테이블 로우 추가 */
         btn_add_row = (Button) findViewById(R.id.btn_add_row);
         btn_add_row.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               add_tableRow();
+                add_tableRow();
             }
         });
+
+        /* 테이블 로우 삭제 */
+        btn_delete_row = (Button) findViewById(R.id.btn_delete_row);
+        btn_delete_row.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                delete_tableRow();
+            }
+        });
+    }
+
+    ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if(result.getResultCode() == RESULT_OK) {
+                    crop_name = intent.getStringExtra("crop_name");
+                    Log.e("TAG", "작물명<< " +crop_name);
+                    textView_crop_name.setText(crop_name);
+                }
+            });
+
+
+    /* 기존 테이블 설정 확인 -> 화면 구성 */
+    void check_table_data() {
+        int count = 0;
+        /*
+            파
+            일
+
+            열
+            어
+            서
+
+            데
+            이
+            터
+            가
+
+            있
+            는
+            지
+
+            확
+            인
+            하
+            고
+
+            개
+            수
+            만
+            큼
+
+            for(int i=0; i<count; i++) {
+                TableRow tableRow = (TableRow) LayoutInflater.from(this).inflate(R.layout.table_setting_add_row, null);
+                ((TextView)tableRow.getChildAt(1)).setText("작물명"); // 작물명 입력
+                ((Spinner)tableRow.getChildAt(2)).setSelection(1); // 숫자 0, 텍스트 1
+                ((CheckBox)tableRow.getChildAt(3)).setChecked(true); // 필수이면 true, 아니면 false
+                table_layout.addView(tableRow);
+            }
+
+            반
+            복
+         */
+
 
     }
 
     /* 테이블 행 추가 */
     void add_tableRow() {
-        DisplayMetrics dm = getApplicationContext().getResources().getDisplayMetrics();
-//        Toast.makeText(this, String.valueOf(dm.widthPixels), Toast.LENGTH_SHORT).show();
-
-        TableRow tableRow = new TableRow(this);
-        tableRow.setBackgroundColor(Color.parseColor("#FFFACD"));
-//        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT
-//                , LinearLayout.LayoutParams.MATCH_PARENT, 1);
-
-        /* 컬럼명 */
-        EditText editText = new EditText(this);
-        editText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20);
-        editText.setWidth(dm.widthPixels/10*4);
-        editText.setSingleLine();
-        tableRow.addView(editText);
-
-        /* 데이터 형식 */
-        Spinner spinner = new Spinner(this);
-        List<String> spinnerList = new ArrayList<String>();
-        spinnerList.add("숫자");
-        spinnerList.add("텍스트");
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, spinnerList);
-        arrayAdapter.setDropDownViewResource(com.google.android.material.R.layout.support_simple_spinner_dropdown_item);
-        spinner.setAdapter(arrayAdapter);
-        tableRow.addView(spinner);
-
-        /* 필수 입력 */
-        CheckBox checkBox = new CheckBox(this);
-        tableRow.addView(checkBox);
-
-        /* 삭제 */
-//        ImageButton button = new ImageButton(this);
-//        button.setImageResource(R.drawable.delete);
-//        button.setBackgroundColor(Color.parseColor("#ffffffff"));
-        Button button = new Button(this);
-        button.setBackgroundResource(R.drawable.delete);
-        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(dm.widthPixels/10, dm.widthPixels/10);
-        button.setLayoutParams(params);
-        tableRow.addView(button);
-
+        TableRow tableRow = (TableRow) LayoutInflater.from(this).inflate(R.layout.table_setting_add_row, null);
         table_layout.addView(tableRow);
-//        table_layout.addView(tableRow, params);
+    }
 
-        /* 삭제 이벤트 */
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                table_layout.removeView(tableRow);
+    /* 테이블 행 삭제 */
+    void delete_tableRow() {
+        ArrayList<Integer> delete_list = new ArrayList<>();
+
+        for(int i=0; i<table_layout.getChildCount(); i++) {
+            TableRow tableRow = (TableRow) table_layout.getChildAt(i);
+            CheckBox checkBox = (CheckBox) tableRow.getChildAt(0);
+            if(checkBox.isChecked()) {
+                delete_list.add(i);
+                Log.e("TAG", i+"행 추가");
             }
-        });
+        }
+
+        int count = 0;
+        for(int i=0; i<delete_list.size(); i++) {
+            Log.e("TAG", delete_list.get(i) +"번째 행 삭제");
+            table_layout.removeViewAt(delete_list.get(i)-count);
+            count++;
+        }
+
+        delete_list.clear();
     }
 }
